fb7930772ca4fce4fe770edc708f1a34eba389fe
Shad0wz93 <Shadowz9@icloud.com>
2022-11-09 14:15:48 +0100

    q

--- addScores.sql
+++ addScores.sql
@@ -1,2 +1,2 @@
 INSERT INTO Scrores (id_utilisateur, id_jeu, difficulty, scrore, `date/heure_partie`)
-VALUES ("3","1","facile","200", "2022&-12-08 03:21:12");
+VALUES ("3","1","facile","200", "20220-12-08 03:21:12");
