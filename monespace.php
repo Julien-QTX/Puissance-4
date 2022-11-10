55f28f52f3ae2c4515b8677c414f83120aa2a262
Julien-QTX <julien.quatravaux@live.fr>
2022-11-10 15:13:41 +0100

    InscriptionS

---
+++ monespace.php
@@ -0,0 +1,96 @@
+<link rel="stylesheet" href="styleProfil.css">
+<?php
+    require "Header.inc.php"
+?>
+<div class="container">
+
+    <h1 class="Titre">Bienvenue <?php $pseudo ?>. Julien</h1>
+
+    <div class="AvaScore">
+
+        <div class="Avatar">
+            <img class="AvaPro" src="./images/Champignon Bleu.jpg"alt="">
+
+            <div class="selecavatar">
+
+                <label for="avatar">Choisir votre photo de profil :</label>
+
+                <input type="file" id="avatar" name="avatar" accept="image/png, image/jpeg" class="avatar">
+
+            </div>
+        </div>
+
+        <div class="Score">
+            <h1 class="Titre">Meilleur Scores</h1>
+            
+            <table>
+                <thead>
+                    <th>scores</th>
+                    <th>Nbr de Coups</th>
+                    <th>Difficulté</th>
+                    <th>Date</th>
+                </thead>
+                <tr>
+                    <td>1</td>
+                    <td>2</td>
+                    <td>3</td>
+                    <td>4</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>2</td>
+                    <td>3</td>
+                    <td>4</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>2</td>
+                    <td>3</td>
+                    <td>4</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>2</td>
+                    <td>3</td>
+                    <td>4</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>2</td>
+                    <td>3</td>
+                    <td>4</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>2</td>
+                    <td>3</td>
+                    <td>4</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>2</td>
+                    <td>3</td>
+                    <td>4</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>2</td>
+                    <td>3</td>
+                    <td>4</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>2</td>
+                    <td>3</td>
+                    <td>4</td>
+                </tr>
+            </table>
+        </div>
+
+    </div>
+
+</div>        
+
+<?php
+    require "Footer.inc.php"
+?>
