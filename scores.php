c9167f15878f2e72ae9413de34e08854cb5b1394
Adam <adamboissy2004@gmail.com>
2023-09-13 15:52:57 +0200

    t

--- scores.php
+++ scores.php
@@ -23,7 +23,7 @@
                     <td>Nom du  jeu</td>
                     <td> pseudo du joueur</td>
                     <td>score dujoueur </td>
-                    <td> Niveau de difhficulté</td>
+                    <td> Niveau de pdifhficulté</td>
                     <td> date/heure</td>        
                 </tr>
             
